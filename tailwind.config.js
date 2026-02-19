b297d798a16f4bb41710ea9a3d0a9247a066ffb1
Damon Phoenix <dev@damonphoenix.com>
2026-02-18 16:41:09 -1000

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./kauai-surf-live-dashboard.html",
+    "./index.html",
+    "./live-kauai-conditions.html",
+    "./**/*.html"
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
